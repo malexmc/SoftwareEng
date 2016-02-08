cd327518c3996a2b0053adcd5d4a70bfab948e48
lambricm <invaderciz@gmail.com>
2016-02-08 13:09:28 -0500

    final touches
    -change/add/remove comments
     deleted commented functions & non-commented functions that were never used
     remove unnecessary "todo"s
    -formatting
     make empty constructor have single line/move comment posision

--- Mapbox/mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/NavigationFragment.java
+++ Mapbox/mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/NavigationFragment.java
@@ -13,7 +13,6 @@ import com.mapbox.mapboxsdk.overlay.Icon;
 import com.mapbox.mapboxsdk.overlay.Marker;
 import com.mapbox.mapboxsdk.views.MapView;
 
-
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
@@ -22,29 +21,22 @@ import com.mapbox.mapboxsdk.views.MapView;
  * Use the {@link NavigationFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+ 
 public class NavigationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
+    
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public NavigationFragment() {
-        // Required empty public constructor
-    }
+    // Required empty public constructor
+    public NavigationFragment() {}
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NavigationFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static NavigationFragment newInstance(String param1, String param2) {
@@ -62,36 +54,13 @@ public class NavigationFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
-            //View navSearchBarHandle = findViewById(R.id.navAddressBar);
-            //navAddressBarHandle.setVisibility(View.VISIBLE);
-
         }
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_navigation, container, false);
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
 
     @Override
     public void onDetach() {
@@ -110,7 +79,6 @@ public class NavigationFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
